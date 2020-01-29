32fe592470ed25e70c0beceb44e50f0386a7ee28
ThiagoDeiro <thiagodeiro.11@hotmail.com>
2020-01-29 20:27:02 +1100

    Updates

---
+++ precache-manifest.1fd62d8072de08e4b0bf1a492425d2b5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "628ee0a181c8b07ba229572dbed239e8",
+    "url": "/porti/index.html"
+  },
+  {
+    "revision": "f04b489fd292c39eceb7",
+    "url": "/porti/static/css/main.740c0417.chunk.css"
+  },
+  {
+    "revision": "b4e8b7249d2ca1fe690b",
+    "url": "/porti/static/js/2.5facd1e2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/porti/static/js/2.5facd1e2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f04b489fd292c39eceb7",
+    "url": "/porti/static/js/main.b957438e.chunk.js"
+  },
+  {
+    "revision": "e568f61db2d4c4bb5eba",
+    "url": "/porti/static/js/runtime-main.fa6ea951.js"
+  },
+  {
+    "revision": "2a6f37b258d146a6e88110b6b601d88f",
+    "url": "/porti/static/media/mealpicker.2a6f37b2.png"
+  },
+  {
+    "revision": "f8d965e5651877deac7e54b209d0f2b3",
+    "url": "/porti/static/media/projectRuby.f8d965e5.png"
+  },
+  {
+    "revision": "a5069a4c6466a70f2fe5700607138070",
+    "url": "/porti/static/media/resume.a5069a4c.pdf"
+  },
+  {
+    "revision": "f1374535535dfbcd4fddd06085c051d7",
+    "url": "/porti/static/media/thiago.f1374535.jpg"
+  },
+  {
+    "revision": "4e61fbd9e0969e0687b86b3bcaa4bfd4",
+    "url": "/porti/static/media/title2.4e61fbd9.jpg"
+  },
+  {
+    "revision": "4ddf5a223494f903fe3049234414ed4a",
+    "url": "/porti/static/media/todolist2.4ddf5a22.png"
+  }
+]);
