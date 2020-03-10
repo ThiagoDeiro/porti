ce53062224eed0d69775e7cd23b80f2c4d937cf4
ThiagoDeiro <thiagodeiro.11@hotmail.com>
2020-03-10 18:29:58 +1100

    card update

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -13,49 +13,37 @@ export default class About extends React.Component {
   render() {
     return (
       <div className="about-main">
-        {/* <header className="about-box"> */}
-        <div className="about-textBox">
-          <div className="img-box">
-            <img alt="" className="user"></img>
-          </div>
-          <h1 className="about-name">About me</h1>
-          <span className="about-text">
-            <p>
-              I'm an enthusiastic front-end developer with a passion for
-              building responsive design and user interfaces.
-            </p>
-            <br />
-            <p>
-              I came upon coding with a head full of inspiration, having seen my
-              previous workplaces transformed by web-based technologies.
-            </p>
-            <p>
-              Recently graduated from a Software Engineering course, I am
-              looking for a company where I can be challenged whilst learning
-              and growing.
-            </p>
-          </span>
+        <img alt="" className="user"></img>
+        <h1 className="about-name">About me</h1>
+        <div className="about-text">
+          <p>
+            I'm an enthusiastic front-end developer with a passion for building
+            responsive design and user interfaces.
+          </p>
+          <br />
+          <p>
+            I came upon coding with a head full of inspiration, having seen my
+            previous workplaces transformed by web-based technologies.
+          </p>
+          <p>
+            Recently graduated from a Software Engineering course, I am looking
+            for a company where I can be challenged whilst learning and growing.
+          </p>
         </div>
 
         <div className="skill-chart">
-          <FaHtml5 className="icons" style={{ color: "#5F6061" }} />
-          {/* rgb(195, 77, 45) */}
-          <FaCss3Alt className="icons" style={{ color: "#5F6061" }} />
-          {/* rgb(55, 76, 218) */}
+          <FaHtml5 className="icons htm" />
 
-          <FaJs className="icons" style={{ color: "#5F6061" }} />
-          {/* rgb(235, 221, 61) */}
+          <FaCss3Alt className="icons cs" />
 
-          <FaReact className="icons" style={{ color: "#5F6061" }} />
-          {/* rgb(140, 211, 242) */}
+          <FaJs className="icons js" />
 
-          <FaNodeJs className="icons" style={{ color: "#5F6061" }} />
-          {/* rgb(148, 187, 44) */}
+          <FaReact className="icons reac" />
 
-          <FaGitAlt className="icons" style={{ color: "#5F6061" }} />
-          {/* rgb(216 , 62, 32) */}
+          <FaNodeJs className="icons node" />
+
+          <FaGitAlt className="icons git" />
         </div>
-        {/* </header> */}
       </div>
     );
   }
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -17,7 +17,8 @@ export default class Projects extends React.Component {
           description: "Web App to search for your favourite movies",
           demo: "https://gaseifx02-project2.herokuapp.com/",
           code: "https://github.com/ThiagoDeiro/gaseifx02-project2",
-          tech: "Ruby on rails, CSS, Bootstrap and MovieDB API"
+          tech:
+            "Ruby on rails, CSS, Bootstrap and MovieDB API,Heroku for deployment,GitHub for version control."
         },
         {
           id: "p2",
@@ -27,7 +28,7 @@ export default class Projects extends React.Component {
           demo: "https://thiagodeiro.github.io/project-meal-picker/",
           code: "https://github.com/ThiagoDeiro/project-meal-picker",
           tech:
-            "React aplication with Rails Backend,CSS,Bootstrap and Zumato API"
+            "React aplication with Rails Backend,CSS,Bootstrap and Zumato API, GitHub for version control."
         },
         {
           id: "p3",
@@ -37,7 +38,7 @@ export default class Projects extends React.Component {
             "ToDo list is a simple app to get your daily tasks in order not to miss anything.",
           demo: "https://thiagodeiro.github.io/gaseifx01-project01/",
           code: "https://github.com/ThiagoDeiro/gaseifx01-project01",
-          tech: "JavaScript,Jquery, CSS"
+          tech: "JavaScript,Jquery, CSS, GitHub for version control."
         },
         {
           id: "p4",
@@ -48,57 +49,49 @@ export default class Projects extends React.Component {
           demo: "https://mockedin.herokuapp.com/",
           code: "https://github.com/ThiagoDeiro/project-3",
           tech:
-            "JavaScript/React for front-end, Node/Express.js for back-end,MongoDB/Mongoose for database,S3/AWS for photos upload,Heroku for deployment,GitHub for version control."
+            "JavaScript/React , Node/Express.js, MongoDB/Mongoose, S3/AWS,Heroku for deployment, GitHub for version control."
         }
       ]
     };
   }
   render() {
     const cards = this.state.projects.map(p => (
-      <div className="card-container" key={p.id}>
-        <div className="divPic">
-          <img src={p.photo} className="image" alt={p.description} />
-        </div>
-        <div className="overlay">
-          <div className="text">{p.title}</div>
-          <div className="ImgContent">
-            <span>
-              {p.description}
-              <br />
-              <span>Tech stack: </span>
-              <br />
-              <span className="techName">{p.tech}</span>
-            </span>
-          </div>
-          <div className="imgSocial">
+      <div className="card" key={p.id}>
+        <h1 className="title">{p.title}</h1>
+        <div className="imgCont">
+          <img className="project-img" src={p.photo} alt={p.title} />
+          <div className="caption">
+            {" "}
             <a
               href={p.demo}
-              className="ImgIcon"
+              className="link"
               target="_blank"
               rel="noopener noreferrer"
             >
               <FaLink></FaLink>
             </a>
+            |{" "}
             <a
               href={p.code}
-              className="ImgIcon"
+              className="link"
               target="_blank"
               rel="noopener noreferrer"
             >
               <FaCode></FaCode>
             </a>
           </div>
+          <div className="desc">
+            <p className="paragraph">{p.description}</p>
+            <p className="paragraph">Tech Stack:</p>
+            <p className="paragraph">{p.tech}</p>
+          </div>
         </div>
       </div>
     ));
     return (
       <div className="projectWrapper">
-        <header className="project-box">
-          <div className="project-textBox">
-            <h1 className="project-name">Projects</h1>
-            <div className="project-text">{cards}</div>
-          </div>
-        </header>
+        <h1 className="page-title">Projects</h1>
+        <div className="sub">{cards}</div>
       </div>
     );
   }
